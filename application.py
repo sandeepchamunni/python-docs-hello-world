b8481ed9f3394495b9d764886505501dad078264
sandeepchamunni <50398671+sandeepchamunni@users.noreply.github.com>
2020-09-05 16:34:33 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -1,6 +1,16 @@
-from flask import Flask
+import spacy
+from flask import Flask, redirect, url_for, request
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/extractcredential", methods=["GET","POST"])
+@cross_origin()
+def extractcredential():
+    nlp2 = spacy.load("custom_ner_model_credential")
+    doc2 = nlp2("Please update the username for the credential XD_FAST_056. The new username is XD_USERNAME and the password is XD$qa123")
+    response = "{"
+    for ent in doc2.ents:
+        response = response + " " + ent.label_ + " " + "," + " " + ent.text + " "
+    return(response)
