559a44ed73bde402d7b3167b0660bd825899ef5c
sandeepchamunni <50398671+sandeepchamunni@users.noreply.github.com>
2020-09-06 14:33:26 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -8,4 +8,18 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/extractcredential", methods=["GET","POST"])
 @cross_origin()
 def extractcredential():
-    return("Hello world")
+    nlp2 = spacy.load("custom_ner_model_credential")
+    CredentialName = ""
+    UserName = ""
+    Password = ""
+    doc2 = nlp2(request.form.get('mailbody'))
+    response = "{"
+    for ent in doc2.ents:
+        if ent.label_ == "CREDENTIAL":
+            CredentialName = ent.text
+        if ent.label_ == "USER":
+            UserName = ent.text
+        if ent.label_ == "PASSWORD":
+            Password = ent.text
+    response = r'{"Credential":"' + CredentialName + '","Username":"' + UserName + '","Password":"' + Password + '"}'
+    return(response)
